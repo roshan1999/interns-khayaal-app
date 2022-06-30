0253ce881163c57a719408a8e972833d13a6ff45
preethisun0801 <preethisun0801@gmail.com>
2022-06-30 15:12:54 +0530

    card carousel for our teams

--- src/Components/TeamsSecComponent/TeamsSecComponent.jsx
+++ src/Components/TeamsSecComponent/TeamsSecComponent.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import Card from "../Card/Card";
 import DividerAndHeading from "../OurTeams/DividerAndHeading";
 import './TeamsSecComponent.css'
-
+import ReactCardCarousel from "react-card-carousel";
 const TeamsSecComponent = (props) => {
     const sectionHeading = props.sectionHeading;
     const [details, setDetails] = useState(props.details)
@@ -17,6 +17,17 @@ const TeamsSecComponent = (props) => {
                 return <Card name={person.name} imageurl={person.imageurl} position={person.position} quote={person.quote} />
             })}
     
+        </div>
+        <div className="mobile-cards">
+            <ReactCardCarousel 
+            autoplay={false}
+          autoplay_speed={2500}
+          spread={"wide"}
+          disable_box_shadow={true}>
+            {details.map((person) => {
+                return <Card name={person.name} imageurl={person.imageurl} position={person.position} quote={person.quote} />
+            })}
+          </ReactCardCarousel>
         </div>
     </div>
     
